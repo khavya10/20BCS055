9c00e19f3322b19e648a9b87fe9545256fa93086
khavya10 <100426269+khavya10@users.noreply.github.com>
2023-06-25 11:54:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,79 @@
-const express=require('express');
-app=express()
-const mongoose=require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/trainbookingsystem')
+const axios = require('axios');
+// const regcomp=async()=>{
+//     try{
+//         const regdat={
+//             compn,
+//             ownn,
+//             rolln,
+//             owem,
+//             accessscode,
+//         }
+//     }
 
-const traindetails=new mongoose.Schema({
-    companyName:String,
-    ownerName:String,
-    rollNo:String,
-    ownerEmail:String
+// }
+const registerCompany = async () => {
+  try {
+    const registrationData = {
+      companyName: 'Train Central',
+      ownerName: 'Rahul',
+      rollNo: '20BCS055',
+      ownerEmail: 'rahul@abc.edu',
+      accessCode: 'nSFqRI',
+    };
 
-})
+    const response = await axios.post('http://104.211.219.98/train/register', registrationData);
+    const { companyName, clientID, clientSecret } = response.data;
 
-const traindetail=mongoose.model('Train',traindetails)
+    console.log('Registration Successful');
+    console.log('Company Name:', companyName);
+    console.log('Client ID:', clientID);
+    console.log('Client Secret:', clientSecret);
 
-const a="";
-const b="";
-const c="";
-const d="";
-const e="";
-// const ip=GET http://104.211.219.98/train/register;
-const myReg = new XMLHttpRequest('http://104.211.219.98/train/auth/');
-// myReg.onload = function( ) {
-//     const data = JSON. parse( this?responseText);
-//     console. log(data);
-// };
-// myReg.onerror = function(err) {
-//     console. log( 'ERROR!" err)
-// };
-// myReg.open( 'get','http://104.211.219.98/train/auth/', true)
-// myReg. setRequestHeader( 'Accept' application/ison')
-// myReg.send()
+    return { clientID, clientSecret };
+  } catch (error) {
+    console.error('Registration failed', error);
+    throw error;
+  }
+};
 
-app.post('/',(req,res)=>{
+const getAuthToken = async ({ clientID, clientSecret }) => {
+  try {
+    const authData = {
+      companyName: 'Train Central',
+      clientID,
+      ownerName: 'Rahul',
+      ownerEmail: 'rahul@abc.edu',
+      rollNo: '20BCS055',
+      clientSecret,
+    };
 
-    var va=new traindetail({
-        myre
+    const response = await axios.post('http://104.211.219.98/train/auth', authData);
+    const { access_token } = response.data;
 
+    return access_token;
+  } catch (error) {
+    console.error('Failed to obtain authorization token', error);
+    throw error;
+  }
+};
 
-    })
-    va.save()
-    console.log(va)
+const fetchTrainDetails = async () => {
+  try {
+    const { clientID, clientSecret } = await registerCompany();
+    const authToken = await getAuthToken({ clientID, clientSecret });
 
-})
+    const response = await axios.get('http://104.211.219.98/train/trains', {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    });
 
-  
- 
+    const trainDetails = response.data;
+    console.log('Train Details:', trainDetails);
+  } catch (error) {
+    console.error('Failed to fetch train details', error);
+    throw error;
+  }
+};
 
-
-
-
-
-
-
-app.listen(3000, () => {
-    console.log(`Example app listening on port `)
-  })
-
-  
+fetchTrainDetails();
